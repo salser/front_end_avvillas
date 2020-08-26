3fe1c4e9f56f1e038c6400369d8b3c181decb7e1
Henry Salazar <henrysteven003@gmail.com>
2020-08-25 22:38:41 -0500

    Mensaje y Video Inicial

--- aOe/src/app/app-routing.module.ts
+++ aOe/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './modules/main-page/main-page.component';
 
+
+const routes: Routes = [
+  { path: '', component: MainPageComponent }, 
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- aOe/src/app/modules/main-page/main-page.component.ts
+++ aOe/src/app/modules/main-page/main-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main-page',
+  selector: 'main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {
+  }
   ngOnInit(): void {
   }
 
+  showFiller = false;
 }
+
+
